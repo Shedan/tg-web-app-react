b80fd1ce9adc0c31751660e4b4c6010543ffaa08
Shedan <shedanua@gmail.com>
2024-06-06 09:29:01 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
 import Header from './components/Header/Header';
 import './App.css';
+import { Route, Routes} from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 
 
 function App() {
@@ -16,8 +19,10 @@ function App() {
   return (
     <div className="App">
         <Header />
-        <button onClick={onToggleButton}>Toggle</button>
-
+          <Routes>
+             <Route index element={<ProductList />}/>
+             <Route path={'form'} element={<Form />}/>
+          </Routes>
     </div>
   );
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+import './Form.css';
+import { useTelegram } from '../../hooks/useTelegram';
+
+const Form = () => {
+ const [name, setName] = useState('');
+ const [phone, setPhone] = useState('');
+ const [city, setCity] = useState('');
+ const {tg} = useTelegram;
+
+
+
+ useEffect (() => {
+    tg.MainButton.setParams({
+        text: 'Відправити данні'
+    })
+ }, [])
+
+ useEffect (() => {
+    if (!name || !phone || !city) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.show();
+    }
+ }, [name,city,phone])
+
+ const onChangeName = (e) =>{
+    setName(e.target.value);
+ }
+
+ const onChangePhone = (e) =>{
+    setPhone(e.target.value);
+ }
+
+ const onChangeCity = (e) =>{
+    setCity(e.target.value);
+ }
+
+
+    return (
+        <div className={"form"}>
+            <h3>Введіть ваші данні</h3>
+            <input type="text" className={'input'} placeholder={"Ім'я"} value={name} onChange={onChangeName} required />
+            <input type="tel" className={'input'} placeholder={"Номер телефону"} value={phone} onChange={onChangePhone} pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required />
+            <input type="text" className={'input'} placeholder={"Місто"} value={city} onChange={onChangeCity} required />
+        </div>
+    );
+};
+
+export default Form;
